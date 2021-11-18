cb37e361f97c53ab4f4621904107c45f169680c6
Meet1103 <meetmehta1103@gmail.com>
2021-11-18 19:45:50 +0530

    Made todo app fully responsive

--- src/App.js
+++ src/App.js
@@ -1,104 +1,147 @@
-import React, { useState, useRef } from "react";            
-import { DragDropContext } from 'react-beautiful-dnd'
-import { v4 as uuidv4 } from 'uuid'
-import TodoForm from './Components/TodoForm/TodoForm'
-import TodoList from './Components/TodoList/TodoList'
+import React, { useState, useEffect, useRef } from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
+import { v4 as uuidv4 } from 'uuid';
+import TodoForm from './Components/TodoForm/TodoForm';
+import TodoList from './Components/TodoList/TodoList';
 import './App.css';
 
+const getTodosFromLocalStorage = () => {
+	let todosArray = localStorage.getItem('todo');
+
+	if (todosArray !== null) {
+		return JSON.parse(todosArray);
+	} else {
+		return [];
+	}
+};
+
 const App = () => {
-  const [name, setName] = useState("");
-  const [todos, setTodos] = useState([]);
-  const inputRef = useRef()
-  const [updateId, setUpdateId] = useState(null)
+	const [name, setName] = useState('');
+	const [todos, setTodos] = useState(
+		getTodosFromLocalStorage()
+	);
+	const [updateId, setUpdateId] = useState(null);
+	const inputRef = useRef();
 
-  function onDragEnd(result) {
-    if (!result.destination) return;
+	useEffect(() => {
+		localStorage.setItem('todo', JSON.stringify(todos));
+	}, [todos, updateId]);
 
-    const reorderedTodoItems = Array.from(todos);
-    const [reorderedItem] = reorderedTodoItems.splice(result.source.index, 1);
-    reorderedTodoItems.splice(result.destination.index, 0, reorderedItem);
+	function onDragEnd(result) {
+		if (!result.destination) return;
 
-    setTodos(reorderedTodoItems);
-  }
+		const reorderedTodoItems = Array.from(todos);
+		const [reorderedItem] = reorderedTodoItems.splice(
+			result.source.index,
+			1
+		);
+		reorderedTodoItems.splice(
+			result.destination.index,
+			0,
+			reorderedItem
+		);
 
-  const handleChange = (e) => {
-    setName(e.target.value);
-  }
+		setTodos(reorderedTodoItems);
+	}
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if(updateId) {
-      const updateTodoValue = todos.find((todo,index) => todo.id === updateId);
-      updateTodoValue.name = name
-      setName("");
-      setUpdateId(null)
-    } else {
-        addTodo(name);
-        setName("");
-    }
-  }
+	const handleChange = (e) => {
+		setName(e.target.value);
+	};
 
-  const inputFocus = () => {
-    inputRef.current.focus()
-  }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (updateId) {
+			const updateTodoValue = todos.find(
+				(todo, index) => todo.id === updateId
+			);
+			updateTodoValue.name = name;
+			setName('');
+			setUpdateId(null);
+		} else {
+			addTodo(name);
+			setName('');
+		}
+	};
 
-  const clearTodo = (e) => {
-    e.preventDefault()
-    setTodos([])
-    inputFocus()
-  }
+	const inputFocus = () => {
+		inputRef.current.focus();
+	};
 
-  const addTodo = (name) => {
-    const newTodo = { id:uuidv4() , name: name, completed: false };
-    setTodos([...todos, newTodo]);
-  }
+	const clearTodo = (e) => {
+		e.preventDefault();
+		setTodos([]);
+		inputFocus();
+	};
 
-  const checkTodo = (id) => {
-    const completedTodo = todos.map((todo,index) => {
-      if (todo.id === id) {
-        return {...todo, completed: !todo.completed}
-      }
-      return todo
-    })
-    setTodos(completedTodo)
-  }
+	const addTodo = (name) => {
+		const newTodo = {
+			id: uuidv4(),
+			name: name,
+			completed: false
+		};
+		setTodos([...todos, newTodo]);
+		inputFocus();
+	};
 
-  const updateTodo = (id) => {
-    const updatedTodo = todos.find((todo,index) => todo.id === id);
-    setName(updatedTodo.name)
-    setUpdateId(updatedTodo.id)
-    inputFocus()
-  }
+	const checkTodo = (id) => {
+		const completedTodo = todos.map((todo, index) => {
+			if (todo.id === id) {
+				return { ...todo, completed: !todo.completed };
+			}
+			return todo;
+		});
+		setTodos(completedTodo);
+	};
 
-  const deleteTodo = (id) => {
-    const remainingTodos = todos.filter((todo,index) => todo.id !== id);
-    setTodos(remainingTodos);
-    inputFocus()
-  } 
+	const updateTodo = (id) => {
+		const updatedTodo = todos.find(
+			(todo, index) => todo.id === id
+		);
+		setName(updatedTodo.name);
+		setUpdateId(updatedTodo.id);
+		inputFocus();
+	};
 
-  return (
-    <div className="App">
-      <header className="heading">TODO APP</header>
-      <TodoForm name={name} updateId={updateId} handleChange={handleChange} handleSubmit={handleSubmit} inputRef={inputRef} />
-      {
-        todos.length > 0 ? (
-          <div>
-            <DragDropContext onDragEnd={onDragEnd} >
-              <TodoList 
-              todos={todos}
-              checkTodo= {checkTodo}
-              updateTodo={updateTodo}
-              deleteTodo={deleteTodo}
-              />
-            </DragDropContext>
-            <button type="submit" onClick={clearTodo} className="clear-todo-list-btn">
-              Clear Todo List
-            </button>
-          </div>
-      ) : <p className="initial-text">No Todo Added</p>
-      }
-    </div>
-  );
-}
+	const deleteTodo = (id) => {
+		const remainingTodos = todos.filter(
+			(todo, index) => todo.id !== id
+		);
+		setTodos(remainingTodos);
+		inputFocus();
+	};
+
+	return (
+		<div className='App'>
+			<header className='heading'>TODO APP</header>
+			<TodoForm
+				name={name}
+				updateId={updateId}
+				handleChange={handleChange}
+				handleSubmit={handleSubmit}
+				inputRef={inputRef}
+			/>
+			{todos.length > 0 ? (
+				<div>
+					<DragDropContext onDragEnd={onDragEnd}>
+						<TodoList
+							todos={todos}
+							checkTodo={checkTodo}
+							updateTodo={updateTodo}
+							deleteTodo={deleteTodo}
+						/>
+					</DragDropContext>
+					<button
+						type='submit'
+						onClick={clearTodo}
+						className='clear-todo-list-btn'>
+						Clear Todo List
+					</button>
+				</div>
+			) : (
+				<p className='initial-text'>No Todo Added</p>
+			)}
+		</div>
+	);
+};
 
 export default App;
--- src/Components/TodoForm/TodoForm.js
+++ src/Components/TodoForm/TodoForm.js
@@ -1,25 +1,33 @@
-import React from "react";
-import "./TodoForm.css"
+import React from 'react'
+import './TodoForm.css'
 
-const TodoForm = ({name, updateId, handleChange, handleSubmit, inputRef}) => {
-  return (
-    <form className="todo-input-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="text"
-        className="todo-input"
-        autoFocus="on"
-        autoComplete="off"
-        ref={inputRef}
-        value={name}
-        onChange={handleChange}
-        required
-      />
-      <button type="submit" className="add-todo-btn">
-        {!updateId ? "Add Todo" : "Save Todo"}
-      </button>
-    </form>
-  );
+const TodoForm = ({
+	name,
+	updateId,
+	handleChange,
+	handleSubmit,
+	inputRef,
+}) => {
+	return (
+		<form
+			className='todo-input-form'
+			onSubmit={handleSubmit}>
+			<input
+				type='text'
+				name='text'
+				className='todo-input'
+				autoFocus='on'
+				autoComplete='off'
+				ref={inputRef}
+				value={name}
+				onChange={handleChange}
+				required
+			/>
+			<button type='submit' className='add-todo-btn'>
+				{!updateId ? 'Add Todo' : 'Save Todo'}
+			</button>
+		</form>
+	)
 }
 
-export default TodoForm;
+export default TodoForm
